881c52f349a5342db47520ea2c0f94f1ec9111d6
CasualSD <93326155+CasualSD@users.noreply.github.com>
2021-11-29 21:52:16 +0100

    11/29/2021

--- Unit 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float xBound = 50f;
+    private float TopxBound = 30f;
+    private float BottomxBound = -10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > xBound || transform.position.z < -xBound)
+        if (transform.position.z > TopxBound)
         {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < BottomxBound)
+        {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Unit 2/Assets/Scripts/SpawnManager.cs
+++ Unit 2/Assets/Scripts/SpawnManager.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    private float randomPosX = 20f;
+    private float spawnPosZ = 20f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 2f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-randomPosX, randomPosX), 0, spawnPosZ);
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
